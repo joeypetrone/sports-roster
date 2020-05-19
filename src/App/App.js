f3df08b67caf310d5535366e3cf494304469b1ed
Joey Petrone <joey.petrone@gmail.com>
2020-05-18 21:05:41 -0500

    components complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.scss';
+import TeamComponent from '../components/TeamComponent/TeamComponent';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+import Auth from '../components/Auth/Auth';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h2 className="mt-3">Sports Roster React App</h2>
-        <button className="btn btn-info mt-3">The Info</button>
+        <MyNavbar />
+        <h2 className="mt-3">Sports Roster</h2>
+        <Auth />
+        <TeamComponent />
       </div>
     );
   }
